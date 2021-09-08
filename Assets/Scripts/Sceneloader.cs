cf20998d047ac70f019f019a8610f6de7c15a531
MykytaHefsymanskyi98 <mykytahefsymanskyi@gmail.com>
2021-09-08 14:10:08 +0300

    Added Sceneloader object and script

---
+++ Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+    int currentSceneIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
