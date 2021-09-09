36ca3426308ff3dfe15a4a51d19e1d45fd7dc2d9
MykytaHefsymanskyi98 <mykytahefsymanskyi@gmail.com>
2021-09-10 00:12:53 +0300

    Added UI Manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,14 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    [Header("UI")]
-    [SerializeField] GameObject mainCanvas;
-    [SerializeField] GameObject pauseCanvas;
-    [SerializeField] Text levelText;
     [Header("Environment objects")]
     [SerializeField] GameObject ball;
     [SerializeField] Transform pivotPoint;
@@ -16,13 +11,14 @@ public class LevelManager : MonoBehaviour
     [SerializeField] GameObject blockMiddle;
     [SerializeField] GameObject blockRight;
     [SerializeField] Defender defender;
+    [SerializeField] UIManager managerUI;
 
     float startingGameSpeed = 1f;
     float currentGameSpeed;
     float nextLevelDelay = 1f;
     int blockCount = 0;
     int ballCount = 0;
-    int currentLevel = 0;
+    int currentLevel = 1;
     string currentLevelText = "Current Level: ";
 
 
@@ -30,54 +26,12 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         currentGameSpeed = startingGameSpeed;
-        PauseCanvasSetActivity(false);
-        MainCanvasSetActivity(true);
-        currentLevel = 1;
-        ShowCurrentLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
        
-    }
-
-    public void StopGame()
-    {
-        currentGameSpeed = 0f;
-        PauseCanvasSetActivity(true);
-        MainCanvasSetActivity(false);
-    }
-
-    public void ResumeGame()
-    {
-        currentGameSpeed = startingGameSpeed;
-        PauseCanvasSetActivity(false);
-        MainCanvasSetActivity(true);
-    }
-
-    void PauseCanvasSetActivity(bool isActive)
-    {
-        if(isActive)
-        {
-            pauseCanvas.SetActive(true);
-        }
-        else
-        {
-            pauseCanvas.SetActive(false);
-        }
-    }
-
-    void MainCanvasSetActivity(bool isActive)
-    {
-        if (isActive)
-        {
-            mainCanvas.SetActive(true);
-        }
-        else
-        {
-            mainCanvas.SetActive(false);
-        }
     }
 
     public void CountBlocks()
@@ -118,17 +72,12 @@ public class LevelManager : MonoBehaviour
     void CreateNewLevel()
     {
         currentLevel++;
-        ShowCurrentLevel();
+        managerUI.ShowCurrentLevel();
         defender.IncreaseSpeed();
         CreateNewBlocks();
     }
 
-    public void ShowCurrentLevel()
-    {
-        levelText.text = currentLevelText + GetCurrentLevel().ToString();
-    }
-
-    int GetCurrentLevel()
+    public int GetCurrentLevel()
     {
         return currentLevel;
     }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] GameObject mainCanvas;
+    [SerializeField] GameObject pauseCanvas;
+    [SerializeField] Text levelText;
+    [Header("References")]
+    [SerializeField] LevelManager levelManager;
+
+    float startingGameSpeed = 1f;
+    float currentGameSpeed;
+    string currentLevelText = "Current Level: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseCanvasSetActivity(false);
+        MainCanvasSetActivity(true);
+        ShowCurrentLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopGame()
+    {
+        currentGameSpeed = 0f;
+        PauseCanvasSetActivity(true);
+        MainCanvasSetActivity(false);
+    }
+
+    public void ResumeGame()
+    {
+        currentGameSpeed = startingGameSpeed;
+        PauseCanvasSetActivity(false);
+        MainCanvasSetActivity(true);
+    }
+
+    void PauseCanvasSetActivity(bool isActive)
+    {
+        if (isActive)
+        {
+            pauseCanvas.SetActive(true);
+        }
+        else
+        {
+            pauseCanvas.SetActive(false);
+        }
+    }
+
+    void MainCanvasSetActivity(bool isActive)
+    {
+        if (isActive)
+        {
+            mainCanvas.SetActive(true);
+        }
+        else
+        {
+            mainCanvas.SetActive(false);
+        }
+    }
+
+    public void ShowCurrentLevel()
+    {
+        levelText.text = currentLevelText + levelManager.GetCurrentLevel().ToString();
+    }
+
+}
