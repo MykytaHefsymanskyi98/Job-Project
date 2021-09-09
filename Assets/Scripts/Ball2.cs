97773dab43cbe85af74a8fa252e7f610705f82c9
MykytaHefsymanskyi98 <mykytahefsymanskyi@gmail.com>
2021-09-09 21:13:04 +0300

    Fixed wall and plane collision issue

---
+++ Assets/Scripts/Ball2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball2 : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float moveSpeed = 500f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TouchPosHandler();
+        //TouchPosHandler();
     }
 
     void TouchPosHandler()
